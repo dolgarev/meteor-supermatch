b6a6a60aebdb8cf25299d832bbc738126bd8504b
dolgarev <o.dolgarev@gmail.com>
2018-12-04 12:21:00 +0200

    bump version to 0.0.4

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'liberation:supermatch',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
